a88c85a5fa2942cf27449df65d8d9e5ffeb52820
Mohit Bisht <mohitbisht1986@gmail.com>
2025-09-04 16:21:20 +0530

    Handled the Captcha

--- tests/utils/env.ts
+++ tests/utils/env.ts
@@ -1,7 +1,7 @@
 export const env = {
-  username: process.env.DEMO_USERNAME ?? '',
-  password: process.env.DEMO_PASSWORD ?? '',
-  gmailUser: process.env.GMAIL_USER ?? '',
-  gmailPassword: process.env.GMAIL_PASSWORD ?? '',
-  demoUrl: process.env.DEMO_URL ?? '',
+  demoUrl: process.env.DEMO_URL ?? 'https://demo.spikerz.com',
+  username: process.env.DEMO_USERNAME ?? 'me',
+  password: process.env.DEMO_PASSWORD ?? 'SmipMe123456',
+  gmailUser: process.env.GMAIL_USER ?? 'spikerztesting@gmail.com',
+  gmailPassword: process.env.GMAIL_PASSWORD ?? 'P@ssw0rd@2708',
 };
